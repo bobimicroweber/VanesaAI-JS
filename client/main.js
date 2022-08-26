ff68544ae0cde011c2aa152b3a039e1c3b692b91
Bozhidar Slaveykov <bobi@microweber.com>
2022-08-26 18:48:27 +0300

    update

--- client/main.js
+++ client/main.js
@@ -1,22 +1,19 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.chatbot.onCreated(function chatbotOnCreated() {
+  this.response = '';
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.chatbot.helpers({
+  response() {
+    return '';
   },
 });
 
-Template.hello.events({
+Template.chatbot.events({
   'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+
   },
 });
